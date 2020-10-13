19d4252ac2930979938da74efa27ad1ac951d1e1
Vrishabh Parmar <vrishabhluvcars@gmail.com>
2020-10-12 21:11:17 -0400

    Updates

---
+++ precache-manifest.3d59a06d774ecc8ce560899475df4e2f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a60a14486a4a86e3bd9cad0b64f1a11a",
+    "url": "/restorante-fusion/index.html"
+  },
+  {
+    "revision": "3d0c307ffb3a248d3163",
+    "url": "/restorante-fusion/static/css/2.2016dff6.chunk.css"
+  },
+  {
+    "revision": "e52d2762c77424820d29",
+    "url": "/restorante-fusion/static/css/main.64c871f9.chunk.css"
+  },
+  {
+    "revision": "3d0c307ffb3a248d3163",
+    "url": "/restorante-fusion/static/js/2.7424b550.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/restorante-fusion/static/js/2.7424b550.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e52d2762c77424820d29",
+    "url": "/restorante-fusion/static/js/main.c148794b.chunk.js"
+  },
+  {
+    "revision": "f0e2f13ec36acfd47726",
+    "url": "/restorante-fusion/static/js/runtime-main.9c0aa3d7.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/restorante-fusion/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/restorante-fusion/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/restorante-fusion/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/restorante-fusion/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/restorante-fusion/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
